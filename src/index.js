6603c7bf4280e4b279a03d02b53207c9750dba70
wangshengliang <1710642275@qq.com>
2020-09-12 20:06:39 +0800

    countdown

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import StudentList from "./components/StudentList";
-const appkey = `1_JavaScript_1564315783200`;
-async function getAllStudent() {
-    return await fetch(`http://open.duyiedu.com/api/student/findAll?appkey=${appkey}`)
-        .then(res => res.json()).then(res => res.data);
-}
-
-async function render(){
-    ReactDOM.render('正在加载中···',document.getElementById('root'));
-    const stus = await getAllStudent();
-    ReactDOM.render(<StudentList stus={stus}/>,document.getElementById('root'));
-}
-render();
-
+import Tick from "./components/Tick";
+ReactDOM.render(<Tick number={10}/>,document.getElementById('root'));
 
 
