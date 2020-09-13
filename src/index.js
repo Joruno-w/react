a0304c514e3e3c819fcbe526b97b6e876a385222
wangshengliang <1710642275@qq.com>
2020-09-13 19:58:30 +0800

    lifeCycle(1)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import Comp from "./components/Comp";
-ReactDOM.render(<Comp/>,document.getElementById('root'));
+ReactDOM.render(<></>,document.getElementById('root'));
