3977d3498cecd09786e12f69b3ad2b9ec0b73e14
wangshengliang <1710642275@qq.com>
2020-09-14 11:17:15 +0800

    deliver element content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-ReactDOM.render(<></>,document.getElementById('root'));
+import Comp from "./Comp";
+ReactDOM.render(<Comp content1={<h1>content1</h1>} content2={<h1>content2</h1>}>
+    这是children属性
+</Comp>,document.getElementById('root'));
