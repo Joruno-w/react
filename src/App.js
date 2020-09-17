cf48d98faae644f55d0f9fc7a33bde90241dd33f
wangshengliang <1710642275@qq.com>
2020-09-17 10:49:56 +0800

    create Portals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
-import Test from "./components/Test";
+import ReactDOM from 'react-dom';
+function A() {
+    return ReactDOM.createPortal(<div>
+        <B/>
+    </div>,document.getElementById('modal'));
+}
+
+function B() {
+    return <div>
+        B
+    </div>
+}
 class App extends Component {
+
     render() {
         return (
-            <Test />
+            <div onClick={()=>{
+                console.log('click');
+            }}>
+                App
+                <A />
+            </div>
         );
     }
 }
