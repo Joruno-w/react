a7d4ca89b7d5eb7e58a69ea96cbe1cd2febdddc1
wangshengliang <1710642275@qq.com>
2020-10-02 16:48:30 +0800

    React events

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, {Component} from 'react';
-import ErrorBound from "./components/common/ErrorBound";
-
-function getDatas() {};
-function Comp1() {
-    const arr = getDatas();
-    const lis = arr.map((it,i)=><li key={i}>{i}</li>)
-    return {lis};
-}
+let prev;
 class App extends Component {
     render() {
         return(
-            <ErrorBound>
-                <Comp1/>
-            </ErrorBound>
-        )
+            <div onClick={e=>{
+                console.log("React事件：div click");
+                console.log(e,prev,prev === e);
+            }}>
+                <button onClick={e=>{
+                    console.log('React事件：button click');
+                    prev = e;
+                    e.persist();
+                    e.nativeEvent.stopImmediatePropagation();
+                    console.log(e.isPropagationStopped());
+                    e.stopPropagation();
+                    console.log(e.isPropagationStopped());
+                }}>click</button>
+            </div>
+        );
     }
+}
+
+document.querySelector("#root").onclick = function (e) {
+    console.log('DOM事件：document click!')
 }
 
 export default App;
