a9d960b157a459cd6fd272b087ca966e97149d10
wangshengliang <1710642275@qq.com>
2020-10-10 13:06:34 +0800

    new node mount

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React, {Component} from 'react';
-let prev;
+
 class App extends Component {
     render() {
-        return(
-            <div onClick={e=>{
-                console.log("React事件：div click");
-                console.log(e,prev,prev === e);
-            }}>
-                <button onClick={e=>{
-                    console.log('React事件：button click');
-                    prev = e;
-                    e.persist();
-                    e.nativeEvent.stopImmediatePropagation();
-                    console.log(e.isPropagationStopped());
-                    e.stopPropagation();
-                    console.log(e.isPropagationStopped());
-                }}>click</button>
+        return (
+            <div>
+                
             </div>
         );
     }
-}
-
-document.querySelector("#root").onclick = function (e) {
-    console.log('DOM事件：document click!')
 }
 
 export default App;
