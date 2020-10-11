74dc93365af73e37539f61caef757f2d724dda6e
wangshengliang <1710642275@qq.com>
2020-10-11 19:50:45 +0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,31 +1,72 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+
+const divRef = React.createRef();
+window.timer = null;
+
+function stop() {
+    clearInterval(window.timer);
+    window.timer = null;
+}
+
+function Move(props) {
+    useEffect(() => {
+        stop();
+        const div = divRef.current;
+        let curTime = 0;
+        let disX = props.left / 100;
+        let disY = props.top / 100;
+        window.timer = setInterval(() => {
+            curTime++;
+            const newLeft = curTime * disX;
+            const newTop = curTime * disY;
+            div.style.left = newLeft + 'px';
+            div.style.top = newTop + 'px';
+            if (curTime === 100) {
+                stop()
+            }
+        }, 1000);
+        return stop;
+    });
+    return (
+        <div ref={divRef} style={{
+            width: 100,
+            height: 100,
+            background: 'red',
+            position: 'fixed',
+            left: 0,
+            top: 0
+        }}></div>
+    )
+}
 
 function App(props) {
-    const [visible, setVisible] = useState(true);
-    const [n, setN] = useState(0);
-    const [,forceUpdate] = useState({});
+    const [position, setPosition] = useState({
+        x: 0,
+        y: 0
+    });
+    const [visible,setVisible] = useState(true);
     return (
         <div>
-            {visible && (<>
-                <button onClick={() => {
-                    setN(n - 1);
-                }}>-
-                </button>
-                <span>{n}</span>
-                <button onClick={() => {
-                    setN(n + 1);
-                }}>+
-                </button>
-            </>)}
-            <p>
-                <button onClick={() => {
-                    setVisible(!visible);
-                }}>显示/隐藏
-                </button>
+            {visible && <Move left={position.x} top={position.y}/>}
+            <div style={{
+                marginLeft: '800px'
+            }}>
+                x: <input type="number" value={position.x} onChange={e => {
+                setPosition({
+                    ...position,
+                    x: parseInt(e.target.value)
+                })
+            }}/>
+                y: <input type="number" value={position.y} onChange={e => {
+                setPosition({
+                    ...position,
+                    y: parseInt(e.target.value)
+                })
+            }}/>
                 <button onClick={()=>{
-                    forceUpdate({});
-                }}>强制刷新</button>
-            </p>
+                    setVisible(!visible);
+                }}>显示/隐藏</button>
+            </div>
         </div>
     );
 }
