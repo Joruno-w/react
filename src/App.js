d91e9fd310dc00784c1c440183e85be8edb82469
wangshengliang <1710642275@qq.com>
2020-10-11 16:42:22 +0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+function App(props) {
+    const [visible, setVisible] = useState(true);
+    const [n, setN] = useState(0);
+    const [,forceUpdate] = useState({});
+    return (
+        <div>
+            {visible && (<>
+                <button onClick={() => {
+                    setN(n - 1);
+                }}>-
+                </button>
+                <span>{n}</span>
+                <button onClick={() => {
+                    setN(n + 1);
+                }}>+
+                </button>
+            </>)}
+            <p>
+                <button onClick={() => {
+                    setVisible(!visible);
+                }}>显示/隐藏
+                </button>
+                <button onClick={()=>{
+                    forceUpdate({});
+                }}>强制刷新</button>
+            </p>
+        </div>
+    );
 }
 
 export default App;
