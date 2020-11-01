29fc2df473bbce03cbfd39995b397967cb96925c
wangshengliang <1710642275@qq.com>
2020-11-01 21:37:24 +0800

    useRef

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import React, {useMemo, useState} from 'react';
-function Item(props){
-    console.log('item render', props.value);
-    return <li>{props.value}</li>
-}
-
+import React, {useEffect, useRef, useState} from 'react';
 function App() {
-    const [range] = useState({min: 1,max: 1000});
-    const [n,setN] = useState(0);
-    const list = useMemo(()=>{
-        const list = [];
-        for (let i = range.min;i <= range.max;i ++){
-            list.push(<Item value={i} key={i}/>);
+    const [n,setN] = useState(10);
+    const nRef = useRef(n); //{current: 10}
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            nRef.current--;
+            setN(nRef.current);
+            if (nRef.current === 0){
+                clearInterval(timer);
+            }
+        },1000);
+        return ()=>{
+            clearTimeout(timer);
         }
-        return list;
-    },[range.min,range.max]);
+    },[]);
     return (
         <div>
-            <ul>
-                {list}
-                <input type="number" value={n} onChange={e=>{
-                    setN(parseInt(e.target.value));
-                }}/>
-            </ul>
+            {n}
         </div>
     );
 }
 
+// function App() {
+//     const [n, setN] = useState(10);
+//     const timer = useRef();
+//     useEffect(() => {
+//         if (n === 0){
+//             return;
+//         }
+//         timer.current = setTimeout(()=>{
+//             console.log(n);
+//             setN(n-1);
+//         },1000);
+//         return ()=>{
+//             clearTimeout(timer.current);
+//         }
+//     }, [n]);
+//     return(
+//         <div>
+//             {n}
+//         </div>
+//     )
+// }
 export default App;
