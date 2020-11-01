d8f56dbeab6f3a0e66fb69c5a24217ae7a97367e
wangshengliang <1710642275@qq.com>
2020-11-01 19:56:14 +0800

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React,{useContext} from 'react';
-const ctx = React.createContext();
-
-function Test() {
-    const value = useContext(ctx);
-    return <h1>上下文中的数据为: {value}</h1>
+import React, {useCallback, useState} from 'react';
+class Test extends React.PureComponent{
+    render() {
+        console.log("Test render");
+        return(
+            <div>
+                <div>{this.props.txt}</div>
+                <button onClick={this.props.onClick}>change</button>
+            </div>
+        )
+    }
 }
-
 function App() {
+    console.log("App render");
+    const [txt,setTxt] = useState(123);
+    const [n,setN] = useState(0);
+    const handleClick = useCallback(()=>{
+        setTxt(txt+1)
+    },[txt]);
     return (
-        <ctx.Provider value="123">
-            <Test/>
-        </ctx.Provider>
+        <div>
+            <Test txt={txt} onClick={handleClick}/>
+            <input type="number" value={n} onChange={e=>{
+                setN(parseInt(e.target.value));
+            }}/>
+        </div>
     );
 }
 
