3f6dc4e98f7bc3662dae9ec8a695c52b0ff0b8ba
wangshengliang <1710642275@qq.com>
2020-11-01 19:16:09 +0800

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
-function App(props) {
-    return (
-       <div>
+import React,{useState} from 'react';
 
-       </div>
+function useReducer(reducer,initialState,func){
+    const [state,setState] = useState(func? func(initialState) : initialState);
+    function dispatch(action){
+        const newState = reducer(state,action);
+        setState(newState);
+    }
+    return [state,dispatch];
+}
+
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'increase':
+            return state + 1;
+        case 'decrease':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+function App() {
+    const [state, dispatch] = useReducer(reducer, 10, (arg)=>{
+        console.log(arg);
+        return 100;
+    });
+    return (
+        <div>
+            <button onClick={()=>{
+                dispatch({type: 'decrease'});
+            }}>-</button>
+            <span>{state}</span>
+            <button onClick={()=>{
+                dispatch({type: 'increase'});
+            }}>+</button>
+        </div>
     );
 }
 
--- src/components/usePageStudent.js
+++ src/components/usePageStudent.js
@@ -11,7 +11,6 @@ function usePageStudent() {
             setResp(result);
         })();
     },[]);
-    return resp;
 }
 
 export default usePageStudent;
