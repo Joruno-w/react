57bfd66536fb6e77fd62f144c738f600d145a59c
wangshengliang <1710642275@qq.com>
2020-11-02 17:06:08 +0800

    react-transition(3)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
 import React, {useState} from 'react';
-import {CSSTransition} from 'react-transition-group';
+import {SwitchTransition,CSSTransition} from 'react-transition-group';
 import './App.css';
 import 'animate.css';
 const duration = 1000;
-function MyTransition({visible,children}) {
-    return <CSSTransition in={visible} timeout={duration} appear mountOnEnter unmountOnExit classNames={{
-        enterActive: 'animate__fadeInRight',
-        appearActive: 'animate__fadeInRight',
-        exitActive: 'animate__fadeOutLeft',
-        exitDone: 'exit-done'
-    }}>
-        {children}
-    </CSSTransition>
-}
-
-
-function Comp1() {
-    return <h1 className="title animate__animated">Comp1</h1>
-}
-function Comp2() {
-    return <h1 className="title animate__animated">Comp2</h1>
-}
-
 function App() {
     const [visible,setVisible] = useState(true);
     return (
         <div className="container">
-            <div>
-                <MyTransition visible={visible}>
-                    <Comp1/>
-                </MyTransition>
-                <MyTransition visible={!visible}>
-                    <Comp2/>
-                </MyTransition>
-            </div>
+            <SwitchTransition>
+                <CSSTransition key={visible} timeout={duration} appear mountOnEnter unmountOnExit
+                               classNames={{
+                                   enter: 'animate__bounceIn',
+                                   exit: 'animate__bounceOut',
+                                   appear: 'animate__bounceIn',
+                                   exitDone: 'exit-done'
+                               }}
+                >
+                    <h1 className="animate__animated">{visible?'title1':'title2'}</h1>
+                </CSSTransition>
+            </SwitchTransition>
             <button onClick={()=>setVisible(!visible)}>click to toggle</button>
         </div>
     );
