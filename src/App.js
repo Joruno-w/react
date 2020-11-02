8be8dd66453837c11f5fd58d9dffc0f201a43a5c
wangshengliang <1710642275@qq.com>
2020-11-02 14:03:04 +0800

    react-transition(1)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import React, {useDebugValue, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {Transition} from 'react-transition-group';
+// import './App.css';
+// function Transition(props) {
+//     const [state,setState] = useState('exited');
+//     useEffect(()=>{
+//         if (props.in){
+//             setState("entering");
+//             setTimeout(()=>{
+//                 setState("entered");
+//             },props.timeout);
+//         }else{
+//             setState('exiting');
+//             setTimeout(()=>{
+//                 setState("exited");
+//             },props.timeout);
+//         }
+//     },[props.in,props.timeout]);
+//     return props.children(state);
+// }
+const duration = 2000;
 
-function useTest() {
-    const [students] = useState([]);
-    useDebugValue("学生集合");
-    return students;
-}
+const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
+};
 
+const transitionStyles = {
+    entering: { opacity: 1 },
+    entered:  { opacity: 1 },
+    exiting:  { opacity: 0 },
+    exited:  { opacity: 0 },
+};
 function App() {
-    useState('123');
-    useEffect(()=>{});
-    useState("abc");
-    useTest();
+    const [inProp,setInProp] = useState(false);
     return (
-        <div>
-
-        </div>
+        <>
+            <Transition in={inProp} timeout={duration} appear mountOnEnter unmountOnExit>
+                {state=>{
+                    console.log(state);
+                    return <div style={{
+                        ...defaultStyle,
+                        ...transitionStyles[state]
+                    }} >
+                        React
+                    </div>
+                }}
+            </Transition>
+            <button onClick={()=>setInProp(!inProp)}>click to toggle</button>
+        </>
     );
 }
 
