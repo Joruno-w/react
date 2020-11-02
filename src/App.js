34f00272481abab76615655a6d84651511472af8
wangshengliang <1710642275@qq.com>
2020-11-02 15:00:28 +0800

    react-transition(2)

--- src/App.js
+++ src/App.js
@@ -1,53 +1,41 @@
-import React, {useEffect, useState} from 'react';
-import {Transition} from 'react-transition-group';
-// import './App.css';
-// function Transition(props) {
-//     const [state,setState] = useState('exited');
-//     useEffect(()=>{
-//         if (props.in){
-//             setState("entering");
-//             setTimeout(()=>{
-//                 setState("entered");
-//             },props.timeout);
-//         }else{
-//             setState('exiting');
-//             setTimeout(()=>{
-//                 setState("exited");
-//             },props.timeout);
-//         }
-//     },[props.in,props.timeout]);
-//     return props.children(state);
-// }
-const duration = 2000;
+import React, {useState} from 'react';
+import {CSSTransition} from 'react-transition-group';
+import './App.css';
+import 'animate.css';
+const duration = 1000;
+function MyTransition({visible,children}) {
+    return <CSSTransition in={visible} timeout={duration} appear mountOnEnter unmountOnExit classNames={{
+        enterActive: 'animate__fadeInRight',
+        appearActive: 'animate__fadeInRight',
+        exitActive: 'animate__fadeOutLeft',
+        exitDone: 'exit-done'
+    }}>
+        {children}
+    </CSSTransition>
+}
 
-const defaultStyle = {
-    transition: `opacity ${duration}ms ease-in-out`,
-    opacity: 0,
-};
 
-const transitionStyles = {
-    entering: { opacity: 1 },
-    entered:  { opacity: 1 },
-    exiting:  { opacity: 0 },
-    exited:  { opacity: 0 },
-};
+function Comp1() {
+    return <h1 className="title animate__animated">Comp1</h1>
+}
+function Comp2() {
+    return <h1 className="title animate__animated">Comp2</h1>
+}
+
 function App() {
-    const [inProp,setInProp] = useState(false);
+    const [visible,setVisible] = useState(true);
     return (
-        <>
-            <Transition in={inProp} timeout={duration} appear mountOnEnter unmountOnExit>
-                {state=>{
-                    console.log(state);
-                    return <div style={{
-                        ...defaultStyle,
-                        ...transitionStyles[state]
-                    }} >
-                        React
-                    </div>
-                }}
-            </Transition>
-            <button onClick={()=>setInProp(!inProp)}>click to toggle</button>
-        </>
+        <div className="container">
+            <div>
+                <MyTransition visible={visible}>
+                    <Comp1/>
+                </MyTransition>
+                <MyTransition visible={!visible}>
+                    <Comp2/>
+                </MyTransition>
+            </div>
+            <button onClick={()=>setVisible(!visible)}>click to toggle</button>
+        </div>
     );
 }
 
