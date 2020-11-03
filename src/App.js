b2e01dd7fe527bf03a54fcbf365b5fd8a9f9275f
wangshengliang <1710642275@qq.com>
2020-11-03 10:48:54 +0800

    react-transition(4)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, {useState} from 'react';
-import {SwitchTransition,CSSTransition} from 'react-transition-group';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 import './App.css';
 import 'animate.css';
+import {v4 as uuid} from 'uuid';
+
 const duration = 1000;
+
 function App() {
-    const [visible,setVisible] = useState(true);
+    const [students, setStudents] = useState([
+        {id: uuid(), name: '任务1'},
+        {id: uuid(), name: '任务2'},
+        {id: uuid(), name: '任务3'},
+    ]);
     return (
-        <div className="container">
-            <SwitchTransition>
-                <CSSTransition key={visible} timeout={duration} appear mountOnEnter unmountOnExit
-                               classNames={{
-                                   enter: 'animate__bounceIn',
-                                   exit: 'animate__bounceOut',
-                                   appear: 'animate__bounceIn',
-                                   exitDone: 'exit-done'
-                               }}
-                >
-                    <h1 className="animate__animated">{visible?'title1':'title2'}</h1>
+        <TransitionGroup component="ul">
+            {students.map(it => (
+                <CSSTransition appear key={it.id} timeout={duration}>
+                    <li>
+                        {it.name}
+                        <button onClick={() => {
+                            setStudents(students.filter(t => t.id !== it.id));
+                        }}>删除
+                        </button>
+                    </li>
                 </CSSTransition>
-            </SwitchTransition>
-            <button onClick={()=>setVisible(!visible)}>click to toggle</button>
-        </div>
+            ))}
+            <button onClick={() => {
+                const name = window.prompt("请输入任务名称");
+                setStudents([...students, {id: uuid(), name}]);
+            }}>添加任务</button>
+        </TransitionGroup>
     );
 }
 
