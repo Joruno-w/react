a84ed63d4086059e5a616a6229df7a425b857c2c
wangshengliang <1710642275@qq.com>
2020-11-03 12:43:50 +0800

    react-transition(5)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import React, {useState} from 'react';
-import {TransitionGroup, CSSTransition} from 'react-transition-group';
+import React from 'react';
+import {TransitionGroup} from 'react-transition-group';
 import './App.css';
-import 'animate.css';
 import {v4 as uuid} from 'uuid';
+import FadeTransition from "./components/common/FadeTransition";
 
 const duration = 1000;
 
-function App() {
-    const [students, setStudents] = useState([
-        {id: uuid(), name: '任务1'},
-        {id: uuid(), name: '任务2'},
-        {id: uuid(), name: '任务3'},
-    ]);
-    return (
-        <TransitionGroup component="ul">
-            {students.map(it => (
-                <CSSTransition appear key={it.id} timeout={duration}>
-                    <li>
-                        {it.name}
-                        <button onClick={() => {
-                            setStudents(students.filter(t => t.id !== it.id));
-                        }}>删除
-                        </button>
-                    </li>
-                </CSSTransition>
-            ))}
-            <button onClick={() => {
-                const name = window.prompt("请输入任务名称");
-                setStudents([...students, {id: uuid(), name}]);
-            }}>添加任务</button>
-        </TransitionGroup>
-    );
+class App extends React.Component {
+    state = {
+        show: true,
+        tasks: [
+            {id: uuid(), name: '任务1'},
+            {id: uuid(), name: '任务2'},
+            {id: uuid(), name: '任务3'},
+        ]
+    };
+
+    render() {
+        return (
+            <TransitionGroup>
+                {this.state.tasks.map(it=>(
+                    <FadeTransition key={it.id} timeout={duration}>
+                        <li>
+                            {it.name}
+                            <button onClick={()=>{
+                                this.setState({
+                                    tasks: this.state.tasks.filter(t=>t.id!==it.id)
+                                });
+                            }}>删除</button>
+                        </li>
+                    </FadeTransition>
+                ))}
+                <button onClick={()=>{
+                    const name = window.prompt("请输入任务名称") || '默认任务名称';
+                    this.setState({
+                        tasks: [...this.state.tasks,{id: uuid(),name}]
+                    });
+                }}>添加</button>
+            </TransitionGroup>
+        );
+    }
 }
 
 export default App;
