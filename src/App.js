8158665ba75158e2d91ce6fedb59a5e778303ff7
wangshengliang <1710642275@qq.com>
2020-11-07 17:34:42 +0800

    Router(其他组件)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
-import Login from "./components/Login";
-import Admin from "./components/Admin";
+import {BrowserRouter as Router, Route, NavLink, Redirect,Switch} from "react-router-dom";
+import './App.css';
+function A() {
+    return <h1>组件A</h1>
+}
+
+function B() {
+    return <h1>组件B</h1>
+}
+
+function NavBar() {
+    return(
+        <div>
+            <NavLink to='/a' style={{marginRight: 10}} innerRef={node=>{
+                console.log(node);
+            }} activeClassName="selected" activeStyle={{
+                background: '#ccc'
+            }}>去A页</NavLink>
+            <NavLink to='/b' style={{marginRight: 10}}>去B页</NavLink>
+            <NavLink to='/abc'>去其他页</NavLink>
+        </div>
+    )
+}
+
 function App(props) {
     return (
         <Router>
+            <NavBar/>
             <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/" component={Admin} />
+                <Route path='/a' component={A}/>
+                <Route path='/b' component={B}/>
+                <Redirect to='/a' from="/abc" />
             </Switch>
         </Router>
     );
