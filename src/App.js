a263bf1c83ad67802fdec1458b34f0319e956bfb
wangshengliang <1710642275@qq.com>
2020-11-09 16:30:30 +0800

    Router(导航守卫)

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
 import React from 'react';
-import {BrowserRouter as Router,Route,Switch,Link} from "react-router-dom";
-import logininfo from "./utils/logininfo";
-import ProtectedRoute from "./components/ProtectedRoute";
-function Home() {
-    return <h1>首页</h1>
+import {Route,Link} from "react-router-dom";
+import RouteGuard from "./components/RouteGuard";
+
+function A() {
+    return <h1>页面A</h1>
 }
 
-function Login(props) {
-    return <div>
-        <h1>登录页</h1>
-        <button onClick={()=>{
-            logininfo.login();
-            if (props.location.state){
-                props.history.push(props.location.state);
-            }else{
-                props.history.push('/');
-            }
-        }}>登录</button>
-    </div>
-}
-
-function Personal() {
-    return <h1>个人中心</h1>
+function B() {
+    return <h1>页面B</h1>
 }
 
 function App(props) {
     return (
-        <Router>
-            <div>
-                <Link to='/'>首页</Link><br/>
-                <Link to='/login'>登录页</Link><br/>
-                <Link to='/personal'>个人中心</Link><br/>
-            </div>
-            <div>
-                <Switch>
-                    <Route path='/login' component={Login} />
-                    <ProtectedRoute path='/personal' component={Personal}/>
-                    <Route path='/' component={Home}/>
-                </Switch>
-            </div>
-        </Router>
+            <RouteGuard onBeforeChange={(pre,cur,action,commit,unBlock)=>{
+                console.log(`页面从${pre.pathname}到${cur.pathname},方式: ${action},允许跳转`);
+                commit(true);
+                unBlock();
+            }} onChange={(pre,cur,action,unListen)=>{
+                console.log(`页面从${pre.pathname}到${cur.pathname},方式: ${action},允许跳转`);
+                unListen();
+            }}>
+                <ul>
+                    <li><Link to='/a'>页面a</Link></li>
+                    <li><Link to='/b' >页面b</Link></li>
+                </ul>
+                <Route path='/a' component={A}/>
+                <Route path='/b' component={B}/>
+            </RouteGuard>
     );
 }
 
