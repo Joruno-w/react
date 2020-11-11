2dfeb17ad31476e14f5bc470d13e6e2cb02413e0
wangshengliang <1710642275@qq.com>
2020-11-11 12:43:53 +0800

    手写createBrowserHistory

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import App from "./App";
+import './react-router/history/index';
+import './react-router/history/history';
 
 
 ReactDOM.render(<App/>,document.getElementById('root'));
-
-
-window.onpopstate = function () {
-    console.log('123');
-};
