37f1cd865b73520a229ee0860c261f2148f53a17
wangshengliang <1710642275@qq.com>
2020-11-12 21:13:54 +0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch, withRouter,Link,NavLink} from "./react-router-dom";
-
-function Page1() {
-    return <h1>Page1</h1>
-}
-
-function Page2() {
-    return <h1>Page2</h1>
-}
-
+import './redux';
 function App(props) {
-    return <Router>
-        <div>
-            <NavLink to='/page1'>page1</NavLink>
-            <NavLink to='/page2'>page2</NavLink>
-        </div>
-        <Switch>
-            <Route path='/page1' component={Page1}/>
-            <Route path='/page2' component={Page2}/>
-        </Switch>
-    </Router>
+    return (
+        <div></div>
+    );
 }
 
 export default App;
