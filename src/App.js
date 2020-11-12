2232ff8ea9faf326bfdfde6b4c93deb4dcdf9373
wangshengliang <1710642275@qq.com>
2020-11-12 10:30:34 +0800

    手写Route组件

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import {BrowserRouter} from "./react-router-dom";
+import {BrowserRouter as Router,Route} from "./react-router-dom";
+
+function A() {
+    return <h1>页面A</h1>
+}
+
+function B() {
+    return <h1>页面B</h1>
+}
+
+function Change({history}) {
+    return <div>
+        <button onClick={()=>{
+            history.push('/a');
+        }}>去A页</button>
+        <button onClick={()=>{
+            history.push('/b');
+        }}>去B页</button>
+    </div>
+}
 
 function App(props) {
-    return <BrowserRouter basename="/news">
-        wsl
-    </BrowserRouter>
+    return <Router>
+        <Route path='/a' component={A}/>
+        <Route path='/b' component={B}/>
+        <Route component={Change}/>
+    </Router>
 }
 
 export default App;
