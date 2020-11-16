72be77aa41a4c275a82cb4c127d68fb42dced686
wangshengliang <1710642275@qq.com>
2020-11-16 08:38:53 +0800

    Redux中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,23 @@
-import {createStore,bindActionCreators} from "../redux";
+import {createStore,bindActionCreators,applyMiddleware} from "redux";
 import reducer from './reducer';
 import * as usersAction from './action/usersAction';
 import {v4 as uuid} from "uuid";
 
-const store = createStore(reducer);
+const logger1 = store=>next=>action=>{
+    console.log("旧状态:",store.getState());
+    next(action);
+    console.log("新状态:",store.getState());
+}
+
+const logger2 = store=>next=>action=>{
+    console.log("旧状态:",store.getState());
+    next(action);
+    console.log("新状态:",store.getState());
+}
+
+
+// const store = createStore(reducer,applyMiddleware(logger1,logger2));
+const store = applyMiddleware(logger1,logger2)(createStore)(reducer);
 store.subscribe(()=>{
     console.log(store.getState());
 });
